9a11e24dd2f2ad538d2d417cd017d3ce002eada3
Deepanshu9045 <rdeepanshu170@gmail.com>
2026-03-01 23:23:52 +0530

    moveAllZeroestoEnd

---
+++ moveAllZeroestoEnd.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void pushZerosToEnd(vector<int>& arr) {
+    int n = arr.size();
+    int j = 0;  
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+    while(j < n) {
+        arr[j] = 0;
+        j++;
+    }
+}
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter elements: \n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    pushZerosToEnd(arr);
+    cout << "Array after moving zeros to end: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
